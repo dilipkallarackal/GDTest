8b2157d2eaf1182bb55d3b0b6aadd7f68037d167
dilipkallarackal <81642393+dilipkallarackal@users.noreply.github.com>
2021-05-13 19:51:14 +0530

    Add files via upload

---
+++ New.py
@@ -0,0 +1,60 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+
+"""
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+"""
+
+"""
+fruits = ["apple", "banana", "cherry"]
+fruitsTuple=fruits
+x, y, z = fruitsTuple
+print(x)
+print(y)
+print(z)
+"""
+
+"""
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+"""
+
+"""
+while True:
+    try:
+        FirstNumber=float(input("Enter the first Value :"))
+    except ValueError:
+        print("Invalid value entered, Please enter again...")
+        continue
+    else:
+        break
+
+while True:
+    try:
+        SecondNumber=float(input("Enter the second Value :"))
+    except ValueError:
+        print("Invalid value entered, Please enter again...")
+        continue
+    else:
+        break
+
+print("Sum of the 2 Values : " + str(Sum))
+
+Sum = float(FirstNumber) + float(SecondNumber)
+"""
+
+
---
+++ T.py
@@ -0,0 +1,16 @@
+class MotorCar(object):
+    def __init__(self):
+        """Base class constructor"""
+
+
+class Car(MotorCar):
+    def __init__(self):
+        """Class constructor"""
+        super(Car, self).__init__()
+
+    def drive(self):
+        print("zOOOOOOmmmmmmmmmmmmmm")
+        """Drive the car"""
+
+red_car = Car()
+red_car.drive()
